6c52a1c75fee9237af20088a5bf8b691c464d965
lidyciya <lidaponarina@yandex.com>
2025-05-29 23:22:04 +0700

    add musicians paginations

--- bands/views.py
+++ bands/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from bands.models import Musician
+from django.core.paginator import Paginator
 
 # Create your views here.
 def musician(request, musician_id):
@@ -10,5 +11,17 @@ def musician(request, musician_id):
     return render(request, "musician.html", data)
 
 def musicians(request):
-    data = {'musicians': Musician.objects.all().order_by("last_name"), }
+    all_musicians = Musician.objects.all().order_by("last_name")
+    paginator = Paginator(all_musicians, 2)
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginatoe.num_pages
+    page = paginator.page(page_num)
+    data = {
+        'musicians': page.object_list,
+        'page': page,
+    }
     return render(request, "musicians.html", data)
